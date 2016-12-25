f21f04a41efec35d5766855010415bec29de2bdf
Krzysztof Gwóźdź <kgwozdz@coders.center>
2016-12-25 22:05:09 +0100

    Add characters, owned items and idicators repositories.

---
+++ ProgrammingGame/src/ProgrammingGame.Data/Repositories/Instances/OwnedItemsRepository.cs
@@ -0,0 +1,11 @@
+﻿using ProgrammingGame.Data.Domain.Entities;
+using ProgrammingGame.Data.Infrastructure.Data;
+using ProgrammingGame.Data.Repositories.Base;
+using ProgrammingGame.Data.Repositories.Interfaces;
+
+namespace ProgrammingGame.Data.Repositories.Instances
+{
+    public class OwnedItemsRepository : GenericRepository<ProgrammingGameContext, OwnedItem>, IOwnedItemsRepository
+    {
+    }
+}
---
+++ ProgrammingGame/src/ProgrammingGame.Data/Repositories/Interfaces/IIndicatorsRepository.cs
@@ -0,0 +1,9 @@
+﻿using ProgrammingGame.Data.Domain.Entities;
+using ProgrammingGame.Data.Repositories.Base;
+
+namespace ProgrammingGame.Data.Repositories.Interfaces
+{
+    public interface IIndicatorsRepository : IGenericRepository<Indicator>
+    {
+    }
+}
