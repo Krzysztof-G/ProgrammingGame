c8aa2684c7caace1560e4cfc9927a60fc06d65de
Krzysztof Gwóźdź <kgwozdz@coders.center>
2016-12-29 18:04:13 +0100

    Add system controller

---
+++ ProgrammingGame/src/ProgrammingGame.Api/Controllers/SystemController.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProgrammingGame.Api.Controllers
+{
+    public class SystemController : BaseController
+    {
+        public SystemController(IMapper mapper) : base(mapper)
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok("Pong");
+        }
+    }
+}
