c2a42732027f647d9df5be356111ac4034b1500b
Krzysztof Gwóźdź <kgwozdz@coders.center>
2017-01-01 22:13:55 +0100

    Add Last State Change Time to Character entity

--- ProgrammingGame/src/ProgrammingGame.Data/Entities/Character.cs
+++ ProgrammingGame/src/ProgrammingGame.Data/Entities/Character.cs
@@ -9,6 +9,7 @@ namespace ProgrammingGame.Data.Entities
         public Guid Key { get; set; }
         public string Name { get; set; }
         public int State { get; set; }
+        public DateTime LastStateChangeTime { get; set; }
         public int Level { get; set; }
         public long Experience { get; set; }
         public long Cash { get; set; }
@@ -17,6 +18,7 @@ namespace ProgrammingGame.Data.Entities
         public User User { get; set; }
 
         public ICollection<Indicator> Indicators { get; set; }
-        public ICollection<OwnedItem> OwnedItems { get; set; }
+        public ICollection<OwnedItem> OwnedItems { get; set; }  
+        public ICollection<SystemAction> SystemActions { get; set; }
     }
 }
---
+++ ProgrammingGame/src/ProgrammingGame.Data/Migrations/20170101211302_AddLastStateChangeTimeToCharacter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProgrammingGame.Data.Migrations
+{
+    public partial class AddLastStateChangeTimeToCharacter : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastStateChangeTime",
+                table: "Characters",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastStateChangeTime",
+                table: "Characters");
+        }
+    }
+}
