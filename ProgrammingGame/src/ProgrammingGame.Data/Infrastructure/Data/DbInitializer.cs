ee28b1040fedb8dc294bbeee92d1cc9a8a1023c3
Krzysztof Gwóźdź <kgwozdz@coders.center>
2017-01-02 00:30:03 +0100

    Add db initialization with enity types

--- ProgrammingGame/src/ProgrammingGame.Data/Infrastructure/Data/DbInitializer.cs
+++ ProgrammingGame/src/ProgrammingGame.Data/Infrastructure/Data/DbInitializer.cs
@@ -1,10 +1,56 @@
-﻿namespace ProgrammingGame.Data.Infrastructure.Data
+﻿using ProgrammingGame.Common.Enums;
+using System;
+using System.Linq;
+using ProgrammingGame.Data.Entities;
+
+namespace ProgrammingGame.Data.Infrastructure.Data
 {
     public static class DbInitializer
     {
         public static void Initialize(ProgrammingGameContext context)
         {
             context.Database.EnsureCreated();
+
+            if (!context.IndicatorTypes.Any())
+            {
+                context.IndicatorTypes.Add(new IndicatorType
+                {
+                    Name = IndicatorTypes.Energy.ToString(),
+                    MinValue = 0,
+                    MaxValue = 100,
+                    DefaultValue = 100,
+                });
+
+                context.IndicatorTypes.Add(new IndicatorType
+                {
+                    Name = IndicatorTypes.Thirst.ToString(),
+                    MinValue = 0,
+                    MaxValue = 100,
+                    DefaultValue = 100,
+                });
+
+                context.IndicatorTypes.Add(new IndicatorType
+                {
+                    Name = IndicatorTypes.Hunger.ToString(),
+                    MinValue = 0,
+                    MaxValue = 100,
+                    DefaultValue = 100,
+                });
+
+                context.SaveChanges();
+            }
+
+            if (!context.SystemActionTypes.Any())
+            {
+                foreach (var systemActionTypeName in Enum.GetNames(typeof(SystemActionTypes)))
+                {
+                    context.SystemActionTypes.Add(new SystemActionType
+                    {
+                        Name = systemActionTypeName
+                    });
+                }
+                context.SaveChanges();
+            }
         }
     }
 }
