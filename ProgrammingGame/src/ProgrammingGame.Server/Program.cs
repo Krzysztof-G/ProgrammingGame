9dbbfc6e28a527ea94f8f66c6cc02da4e9af5665
Krzysztof Gwóźdź <kgwozdz@coders.center>
2017-01-01 17:32:39 +0100

    Add Server project

---
+++ ProgrammingGame/src/ProgrammingGame.Server/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace ProgrammingGame.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
