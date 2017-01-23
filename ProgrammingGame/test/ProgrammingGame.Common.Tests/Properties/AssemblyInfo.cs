95fc51b7faa1062e4a1a131ec90b0d74ed4a4d2c
Krzysztof Gwóźdź <kgwozdz@coders.center>
2017-01-23 23:48:20 +0100

    Improvment tests projects

---
+++ ProgrammingGame/test/ProgrammingGame.Common.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProgrammingGame.Common.Tests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d7618df0-d6c0-4353-88c6-f8148d057a0c")]
